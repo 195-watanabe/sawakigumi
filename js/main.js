979fb6de7cc9d362da12a5a2cfcd340bf6503d52
195-watanabe <19n.oshigoto@gmail.com>
2023-03-06 08:34:20 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,92 @@
+// // スムーススクロール
+// var scroll = new SmoothScroll('a[href*="#"]', {
+// speedAsDuration:true,
+// speed:1000,
+// easing:'easeInOutQuad' // イージング
+// });
+
+// ハンバーガーメニュー
+
+$(".news").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('_active');//ボタン自身に activeクラスを付与し
+    $(".gnav").toggleClass('_active');//ナビゲーションにpanelactiveクラスを付与
+});
+
+
+/* ナビゲーション
+-----------------------------*/
+$(window).scroll(function () {
+  $('#company01').each(function () {
+    var hit = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var wHeight = $(window).height();
+    var customTop = 80;
+    if (typeof $(this).data('effect') !== 'undefined') {
+      // data-effect="任意の値" が設定されている場合
+      customTop = $(this).data('effect');
+    }
+    if (hit + customTop < wHeight + scroll) {
+      $('.main_menu li').removeClass("_current");
+      $('#nav01').addClass("_current");
+    }
+  });
+  $('#company02').each(function () {
+    var hit = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var wHeight = $(window).height();
+    var customTop = 80;
+    if (typeof $(this).data('effect') !== 'undefined') {
+      // data-effect="任意の値" が設定されている場合
+      customTop = $(this).data('effect');
+    }
+    if (hit + customTop < wHeight + scroll) {
+      $('.main_menu li').removeClass("_current");
+      $('#nav02').addClass("_current");
+    }
+  });
+  $('#company03').each(function () {
+    var hit = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var wHeight = $(window).height();
+    var customTop = 80;
+    if (typeof $(this).data('effect') !== 'undefined') {
+      // data-effect="任意の値" が設定されている場合
+      customTop = $(this).data('effect');
+    }
+    if (hit + customTop < wHeight + scroll) {
+      $('.main_menu li').removeClass("_current");
+      $('#nav03').addClass("_current");
+    }
+  });
+  $('#company04').each(function () {
+    var hit = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var wHeight = $(window).height();
+    var customTop = 80;
+    if (typeof $(this).data('effect') !== 'undefined') {
+      // data-effect="任意の値" が設定されている場合
+      customTop = $(this).data('effect');
+    }
+    if (hit + customTop < wHeight + scroll) {
+      $('.main_menu li').removeClass("_current");
+      $('#nav04').addClass("_current");
+    }
+  });
+  $('#company05').each(function () {
+    var hit = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var wHeight = $(window).height();
+    var customTop = 80;
+    if (typeof $(this).data('effect') !== 'undefined') {
+      // data-effect="任意の値" が設定されている場合
+      customTop = $(this).data('effect');
+    }
+    if (hit + customTop < wHeight + scroll) {
+      $('.main_menu li').removeClass("_current");
+      $('#nav05').addClass("_current");
+    }
+  });
+});
+
+
+
